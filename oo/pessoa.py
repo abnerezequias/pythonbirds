aec6306626576759dc9ecc8d10850568edaf35bd
abnerezequias <engabner84@gmail.com>
2020-09-12 18:08:27 -0300

    Criados atributos de instância nime e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
+
 if __name__ == '__main__':
-        p = Pessoa()
-        print(Pessoa.cumprimentar(p))
-        print(id(p))
-        print(p.cumprimentar())
+    p = Pessoa('Luciano')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    p.nome = 'Abner'
+    print(p.nome)
